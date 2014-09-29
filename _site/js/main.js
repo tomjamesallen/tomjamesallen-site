8831420de48a0ce10fe0e2b3c35dc2de467d153a
Tom Allen <tomjamesallenweb@gmail.com>
2014-09-29 08:09:23 +0100

    Tweaking min width for parallax to exclude iPad.

--- _site/js/main.js
+++ _site/js/main.js
@@ -2,7 +2,7 @@ var parallax = basicScrollParallax({
   el: '.bg-img',
   posEffectLevel: 0.3,
   enabled: function () {
-    if ($(window).width() < 800) {
+    if ($(window).width() <= 1024) {
       return false;
     }
     else {
